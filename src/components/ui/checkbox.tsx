b1b878c648072ddd2deaa7a630af26b446bde640
vadzim.l <vadzim.l@realmond.com>
2025-04-07 22:25:11 +0300

    fix: checkbox import icon

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -2,9 +2,10 @@
 
 import * as React from 'react';
 import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
-import { Check } from 'lucide-react';
 
 import { cn } from '~/lib/utils';
+
+import { CheckIcon } from './icons/check';
 
 const Checkbox = React.forwardRef<
   React.ElementRef<typeof CheckboxPrimitive.Root>,
@@ -19,7 +20,7 @@ const Checkbox = React.forwardRef<
     {...props}
   >
     <CheckboxPrimitive.Indicator className={cn('flex items-center justify-center text-current')}>
-      <Check className='h-4 w-4 group-data-[state=checked]:block hidden' />
+      <CheckIcon className='h-4 w-4 group-data-[state=checked]:block hidden' />
     </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
 ));
